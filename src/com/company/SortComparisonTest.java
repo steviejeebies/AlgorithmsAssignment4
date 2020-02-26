52f13a4404d7c225d99ef5acf0734d6cff1d8c99
steviejeebies <rowes@tcd.ie>
2020-02-26 02:42:25 +0000

    added questions and some answers from assignment pdf

--- src/com/company/SortComparisonTest.java
+++ src/com/company/SortComparisonTest.java
@@ -25,10 +25,30 @@ import java.util.Arrays;
 
 @RunWith(JUnit4.class)
 public class SortComparisonTest {
-    //~ Constructor ........................................................
-    @Test
-    public void testConstructor() {
-        new SortComparison();
+    public static boolean testAllAlgorithms(double[] arrayToSort) {
+        double[] originalCopy = Arrays.copyOf(arrayToSort, arrayToSort.length);
+        double[] insertionSortResult = SortComparison.insertionSort(originalCopy);
+        double[] result;
+        // For this, we have to assume insertion sort is correct. Since it should match all other
+        // algorithms anyway, and if all tests are passed, then we can also assume insertion
+        // sort is also correct
+
+        result = SortComparison.selectionSort(originalCopy);
+        if (!Arrays.equals(insertionSortResult, result)) return false;
+
+        originalCopy = Arrays.copyOf(arrayToSort, arrayToSort.length);
+        result = SortComparison.mergeSortIterative(originalCopy);
+        if (!Arrays.equals(insertionSortResult, result)) return false;
+
+        originalCopy = Arrays.copyOf(arrayToSort, arrayToSort.length);
+        result = SortComparison.mergeSortRecursive(originalCopy);
+        if (!Arrays.equals(insertionSortResult, result)) return false;
+
+        originalCopy = Arrays.copyOf(arrayToSort, arrayToSort.length);
+        result = SortComparison.quickSort(originalCopy);
+        if (!Arrays.equals(insertionSortResult, result)) return false;
+
+        return true;
     }
 
     //~ Public Methods ........................................................
@@ -36,108 +56,73 @@ public class SortComparisonTest {
     // ----------------------------------------------------------
 
     /**
-     * Check that the methods work for empty arrays
-     */
-    @Test
-    public void testEmpty() {
-        double [] empty = {};
-        double [] emptyCopy = {};
-        double [] result;
-
-        result = SortComparison.insertionSort(empty);
-        Assert.assertArrayEquals(emptyCopy, result, 0);
-        result = SortComparison.selectionSort(empty);
-        Assert.assertArrayEquals(emptyCopy, result, 0);
-        result = SortComparison.mergeSortIterative(empty);
-        Assert.assertArrayEquals(emptyCopy, result, 0);
-        result = SortComparison.mergeSortRecursive(empty);
-        Assert.assertArrayEquals(emptyCopy, result, 0);
-        result = SortComparison.quickSort(empty);
-        Assert.assertArrayEquals(emptyCopy, result, 0);
-    }
-
-    @Test
-    public void testArray1Element() {
-        double [] numbers1 = {1};
-        double [] numbers1Copy = {1};
-        double [] result;
-
-        result = SortComparison.insertionSort(numbers1);
-        Assert.assertArrayEquals(numbers1Copy, result, 0);
-        result = SortComparison.selectionSort(numbers1);
-        Assert.assertArrayEquals(numbers1Copy, result, 0);
-        result = SortComparison.mergeSortIterative(numbers1);
-        Assert.assertArrayEquals(numbers1Copy, result, 0);
-        result = SortComparison.mergeSortRecursive(numbers1);
-        Assert.assertArrayEquals(numbers1Copy, result, 0);
-        result = SortComparison.quickSort(numbers1);
-        Assert.assertArrayEquals(numbers1Copy, result, 0);
-    }
-
-    @Test
-    public void assertAllSortingAlgorithmResultsEqual() {
-
-        double [] numbers1000 = getDoubleArrayFromTextFile("numbers1000.txt", 1000);
-        double [] numbers1000Duplicates = getDoubleArrayFromTextFile("numbers1000Duplicates.txt", 1000);
-        double [] numbersNearlyOrdered1000 = getDoubleArrayFromTextFile("numbersNearlyOrdered1000.txt", 1000);
-        double [] numbersReverse1000 = getDoubleArrayFromTextFile("numbersReverse1000.txt", 1000);
-        double [] numbersSorted1000 = getDoubleArrayFromTextFile("numbersSorted1000.txt", 1000);
-
-        assertTrue(testAllAlgorithms(numbers1000));
-        assertTrue(testAllAlgorithms(numbers1000Duplicates));
-        assertTrue(testAllAlgorithms(numbersNearlyOrdered1000));
-        assertTrue(testAllAlgorithms(numbersReverse1000));
-        assertTrue(testAllAlgorithms(numbersSorted1000));
-    }
-
-    public static boolean testAllAlgorithms(double [] arrayToSort) {
-        double [] originalCopy = Arrays.copyOf(arrayToSort, arrayToSort.length);
-        double [] insertionSortResult = SortComparison.insertionSort(originalCopy);
-        double [] result;
-        // For this, we have to assume insertion sort is correct. Since it should match all other
-        // algorithms anyway, and if all tests are passed, then we can also assume insertion
-        // sort is also correct
-
-        result = SortComparison.selectionSort(originalCopy);
-        if(!Arrays.equals(insertionSortResult, result)) return false;
-
-        originalCopy = Arrays.copyOf(arrayToSort, arrayToSort.length);
-        result = SortComparison.mergeSortIterative(originalCopy);
-        if(!Arrays.equals(insertionSortResult, result)) return false;
-
-        originalCopy = Arrays.copyOf(arrayToSort, arrayToSort.length);
-        result = SortComparison.mergeSortRecursive(originalCopy);
-        if(!Arrays.equals(insertionSortResult, result)) return false;
-
-        originalCopy = Arrays.copyOf(arrayToSort, arrayToSort.length);
-        result = SortComparison.quickSort(originalCopy);
-        if(!Arrays.equals(insertionSortResult, result)) return false;
-
-        return true;
-    }
-
-    /**
      * Main Method.
      * Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
      */
     public static void main(String[] args) {
-        double [] numbers10 = getDoubleArrayFromTextFile("numbers10.txt", 10);
-        double [] numbers100 = getDoubleArrayFromTextFile("numbers100.txt", 100);
-        double [] numbers1000 = getDoubleArrayFromTextFile("numbers1000.txt", 1000);
-        double [] numbers1000Duplicates = getDoubleArrayFromTextFile("numbers1000Duplicates.txt", 1000);
-        double [] numbersNearlyOrdered1000 = getDoubleArrayFromTextFile("numbersNearlyOrdered1000.txt", 1000);
-        double [] numbersReverse1000 = getDoubleArrayFromTextFile("numbersReverse1000.txt", 1000);
-        double [] numbersSorted1000 = getDoubleArrayFromTextFile("numbersSorted1000.txt", 1000);
+        /*
+                EXPERIMENT RESULTS
+                            INS  |  SEL	  | QUICK | MRGR  |  MRGI
+                __________________________________________________
+                10	    |   3133	2566	2000	4533	20400
+                100	    |   40700	69066	34000	48400	78100
+                1000    |	2137033	3077533	779766	423266	212300
+                1000Dup |	4382400	2272333	113766	111966	141166
+                1000NO  |	26200	416033	200133	64266	102800
+                1000Rev |	254666	611466	1634666	62000	146700
+                1000Sort|	2200	410100	1349533	30233	181233
 
-        long [] num10Durations = new long [5];
-        long [] num100Durations = new long [5];
-        long [] num1000Durations = new long [5];
-        long [] num1000DuplicatesDurations = new long [5];
-        long [] num1000NearlyOrderedDurations = new long [5];
-        long [] num1000ReverseDurations = new long [5];
-        long [] num1000SortedDurations = new long [5];
+         *Results are in NanoTime
 
-        for(int i = 0; i < 3; i++) {
+           Answers:
+           A:
+           B: The algorithm with the biggest difference between best and worst case for input size 1000 was
+              insertionSort. For insertion sort, an already sorted array only has to be iterated through once, and
+              each iteration of this loop checks if value n is greater than n - 1. As this is always guaranteed to be true
+              then iterating through the array will be very quick. Where insertionSort fails is when there are many duplicates.
+              Each time a value is pushed back in the array, it must also be pushed back through every value that is equal to it, too.
+              Due to the fact that there are so many duplicates in this array, this means that a lot of time is wasted.
+
+           C: The algorithm with the worst scalability is Selection sort. An input of size 10^3 is 1,999.35 times slow that
+              the same algorithm with a input size of 10^1.
+              The algorithm with the best scalability is Iterative Merge Sort. While in the case of input size 10, Iterative
+              Merge Sort was by far the worst, we do have the option to cut-off to other algorithms for smaller N values.
+              However, the difference between the smallest N value duration and largest N value duration for Iterative
+              Merge Sort is the lowest, and it also produced the best result out of all the algorithms at 1000 N input size.
+
+           D: When I was initially implementing merge sort, I tried to modify the recursive merge() function so that we
+              would have to copy in the elements from the original array into the temp array each time. However, I was
+              unable to implement this with the iterative version of merge sort, and opted to keep the same merge() method
+              for both. In respect to the results, Merge Sort Recursive typically produces faster completion, but Merge Sort
+              iterative is more consistent in how long it takes to sort the array.
+
+            E: For each of the 7 Input files, the fastest were:
+                10	:	Quick
+                100	:	Quick
+                1000	:	MrgIterative
+                1000Dup	:	MrgRecursive
+                1000NO	:	Insertion
+                1000Rev	:	MrgRecursive
+                1000Sort	:	Insertion
+         */
+
+        double[] numbers10 = getDoubleArrayFromTextFile("numbers10.txt", 10);
+        double[] numbers100 = getDoubleArrayFromTextFile("numbers100.txt", 100);
+        double[] numbers1000 = getDoubleArrayFromTextFile("numbers1000.txt", 1000);
+        double[] numbers1000Duplicates = getDoubleArrayFromTextFile("numbers1000Duplicates.txt", 1000);
+        double[] numbersNearlyOrdered1000 = getDoubleArrayFromTextFile("numbersNearlyOrdered1000.txt", 1000);
+        double[] numbersReverse1000 = getDoubleArrayFromTextFile("numbersReverse1000.txt", 1000);
+        double[] numbersSorted1000 = getDoubleArrayFromTextFile("numbersSorted1000.txt", 1000);
+
+        long[] num10Durations = new long[5];
+        long[] num100Durations = new long[5];
+        long[] num1000Durations = new long[5];
+        long[] num1000DuplicatesDurations = new long[5];
+        long[] num1000NearlyOrderedDurations = new long[5];
+        long[] num1000ReverseDurations = new long[5];
+        long[] num1000SortedDurations = new long[5];
+
+        for (int i = 0; i < 3; i++) {
             getDurationAllAlgorithms(numbers10, num10Durations);
             getDurationAllAlgorithms(numbers100, num100Durations);
             getDurationAllAlgorithms(numbers1000, num1000Durations);
@@ -147,15 +132,14 @@ public class SortComparisonTest {
             getDurationAllAlgorithms(numbersSorted1000, num1000SortedDurations);
         }
 
-        for(int i = 0; i < 5; i++)
-        {
-            num10Durations[i] = num10Durations[i]/3;
-            num100Durations[i] = num100Durations[i]/3;
-            num1000Durations[i] = num1000Durations[i]/3;
-            num1000DuplicatesDurations[i] = num1000DuplicatesDurations[i]/3;
-            num1000NearlyOrderedDurations[i] = num1000NearlyOrderedDurations[i]/3;
-            num1000ReverseDurations[i] = num1000ReverseDurations[i]/3;
-            num1000SortedDurations[i] = num1000SortedDurations[i]/3;
+        for (int i = 0; i < 5; i++) {
+            num10Durations[i] = num10Durations[i] / 3;
+            num100Durations[i] = num100Durations[i] / 3;
+            num1000Durations[i] = num1000Durations[i] / 3;
+            num1000DuplicatesDurations[i] = num1000DuplicatesDurations[i] / 3;
+            num1000NearlyOrderedDurations[i] = num1000NearlyOrderedDurations[i] / 3;
+            num1000ReverseDurations[i] = num1000ReverseDurations[i] / 3;
+            num1000SortedDurations[i] = num1000SortedDurations[i] / 3;
         }
 
         System.out.println(PrintResults(num10Durations, "10"));
@@ -169,10 +153,9 @@ public class SortComparisonTest {
         // have to get copy arrays, as some of the algorithms are in-place:
     }
 
-    public static void getDurationAllAlgorithms(double [] array, long [] durationArray)
-    {
-        double [] originalCopy = Arrays.copyOf(array, array.length);
-        double [] result;
+    public static void getDurationAllAlgorithms(double[] array, long[] durationArray) {
+        double[] originalCopy = Arrays.copyOf(array, array.length);
+        double[] result;
         long start;
         long end;
 
@@ -205,18 +188,14 @@ public class SortComparisonTest {
         originalCopy = Arrays.copyOf(array, array.length);
 
 
-
-
     }
 
-    public static String PrintResults(long [] result, String arrayName)
-    {
+    public static String PrintResults(long[] result, String arrayName) {
         return arrayName + ": INS[" + result[0] + "] SEL[" + result[1] + "] QCK[" + result[2] + "] MGR[" + result[3] + "] MGI[" + result[4] + ']';
     }
 
-    public static double [] getDoubleArrayFromTextFile(String fileName, int arraySize)
-    {
-        double [] returnArray = new double[arraySize];
+    public static double[] getDoubleArrayFromTextFile(String fileName, int arraySize) {
+        double[] returnArray = new double[arraySize];
         int i = 0; // array index
         try {
             File file = new File(fileName);    //creates a new file instance
@@ -225,11 +204,71 @@ public class SortComparisonTest {
             while (i < arraySize)
                 returnArray[i++] = Double.parseDouble(ourBufferedReader.readLine());
             ourFileReader.close();    //closes the stream and release the resources
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return returnArray;
+    }
+
+    //~ Constructor ........................................................
+    @Test
+    public void testConstructor() {
+        new SortComparison();
+    }
+
+    /**
+     * Check that the methods work for empty arrays
+     */
+    @Test
+    public void testEmpty() {
+        double[] empty = {};
+        double[] emptyCopy = {};
+        double[] result;
+
+        result = SortComparison.insertionSort(empty);
+        Assert.assertArrayEquals(emptyCopy, result, 0);
+        result = SortComparison.selectionSort(empty);
+        Assert.assertArrayEquals(emptyCopy, result, 0);
+        result = SortComparison.mergeSortIterative(empty);
+        Assert.assertArrayEquals(emptyCopy, result, 0);
+        result = SortComparison.mergeSortRecursive(empty);
+        Assert.assertArrayEquals(emptyCopy, result, 0);
+        result = SortComparison.quickSort(empty);
+        Assert.assertArrayEquals(emptyCopy, result, 0);
+    }
+
+    @Test
+    public void testArray1Element() {
+        double[] numbers1 = {1};
+        double[] numbers1Copy = {1};
+        double[] result;
+
+        result = SortComparison.insertionSort(numbers1);
+        Assert.assertArrayEquals(numbers1Copy, result, 0);
+        result = SortComparison.selectionSort(numbers1);
+        Assert.assertArrayEquals(numbers1Copy, result, 0);
+        result = SortComparison.mergeSortIterative(numbers1);
+        Assert.assertArrayEquals(numbers1Copy, result, 0);
+        result = SortComparison.mergeSortRecursive(numbers1);
+        Assert.assertArrayEquals(numbers1Copy, result, 0);
+        result = SortComparison.quickSort(numbers1);
+        Assert.assertArrayEquals(numbers1Copy, result, 0);
+    }
+
+    @Test
+    public void assertAllSortingAlgorithmResultsEqual() {
+
+        double[] numbers1000 = getDoubleArrayFromTextFile("numbers1000.txt", 1000);
+        double[] numbers1000Duplicates = getDoubleArrayFromTextFile("numbers1000Duplicates.txt", 1000);
+        double[] numbersNearlyOrdered1000 = getDoubleArrayFromTextFile("numbersNearlyOrdered1000.txt", 1000);
+        double[] numbersReverse1000 = getDoubleArrayFromTextFile("numbersReverse1000.txt", 1000);
+        double[] numbersSorted1000 = getDoubleArrayFromTextFile("numbersSorted1000.txt", 1000);
+
+        assertTrue(testAllAlgorithms(numbers1000));
+        assertTrue(testAllAlgorithms(numbers1000Duplicates));
+        assertTrue(testAllAlgorithms(numbersNearlyOrdered1000));
+        assertTrue(testAllAlgorithms(numbersReverse1000));
+        assertTrue(testAllAlgorithms(numbersSorted1000));
     }
 
 }
